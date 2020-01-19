29ace559ca4bb87cb00b12da20b4131e8e6e9956
Kenta Aikawa <kalupas226@gmail.com>
2020-01-19 10:48:13 +0900

    add fishmodel and refactor app and main

---
+++ lib/src/models/fish_model.dart
@@ -0,0 +1,39 @@
+class FishModel {
+
+  FishModel.fromJson(Map<String, dynamic> parsedJson)
+      : id = parsedJson['id'],
+        name = parsedJson['name'],
+        hiraganaName = parsedJson['hiraganaName'],
+        kanziName = parsedJson['kanziName'],
+        areaName = parsedJson['areaName'],
+        phrase = parsedJson['phrase'],
+        description = parsedJson['description'],
+        category = parsedJson['category'],
+        length = parsedJson['length'],
+        area = parsedJson['area'],
+        areaDetail = parsedJson['areaDetail'],
+        catchSeason = parsedJson['catchSeason'],
+        catchSeasonDetail = parsedJson['catchSeasonDetail'],
+        rare = parsedJson['rare'],
+        url = parsedJson['url'],
+        imageUrl = parsedJson['imageUrl'],
+        aname = parsedJson['aname'];
+
+  final int id;
+  final String name;
+  final String hiraganaName;
+  final String kanziName;
+  final String areaName;
+  final String phrase;
+  final String description;
+  final String category;
+  final String length;
+  final String area;
+  final String areaDetail;
+  final String catchSeason;
+  final String catchSeasonDetail;
+  final String rare;
+  final String url;
+  final String imageUrl;
+  final String aname; // 何なのか不明
+}
