68cf051d8c64c3ddfa92863e08698ed2296948a3
Kenta Aikawa <kalupas226@gmail.com>
2020-01-20 12:40:22 +0900

    modify repository

--- lib/src/resources/repository.dart
+++ lib/src/resources/repository.dart
@@ -1,8 +1,14 @@
-import 'fish_api_provider.dart';
 import '../models/fish_model.dart';
+import '../models/recipe_model.dart';
+import 'fish_api_provider.dart';
+import 'recipe_api_provider.dart';
 
 class Repository {
   Future<Map<int, FishModel>> fetchFishes() {
     return FishApiProvider().fetchFishes();
+  }
+
+  Future<Map<int, RecipeModel>> fetchRecipes() {
+    return RecipeApiProvider().fetchRecipes();
   }
 }
